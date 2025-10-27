f1431a6b4d97b6c110d6c0db79732201c93dca98
Juanjo200 <131538215+Juanjo200@users.noreply.github.com>
2025-10-26 20:12:20 -0500

    feat: level 2 and test

--- students/s-juanjo/src/level1.js
+++ students/s-juanjo/src/level1.js
@@ -1,12 +1,7 @@
-/**
- * Level 1 - Basic Functions
- * Student: s-student7
- */
-
 // --- Function 1 ---
 function alternateCase(str) {
-  if (typeof str !== 'string') throw new Error('Input must be a string');
-  let result = '';
+  if (typeof str !== "string") throw new Error("Input must be a string");
+  let result = "";
   let uppercase = true;
   for (const char of str) {
     if (char.match(/[a-z]/i)) {
@@ -21,14 +16,14 @@ function alternateCase(str) {
 
 // --- Function 2 ---
 function productOfArray(arr) {
-  if (!Array.isArray(arr)) throw new Error('Input must be an array');
+  if (!Array.isArray(arr)) throw new Error("Input must be an array");
   return arr.reduce((product, num) => product * num, 1);
 }
 
 // --- Function 3 ---
 function filterNegatives(arr) {
-  if (!Array.isArray(arr)) throw new Error('Input must be an array');
-  return arr.filter(num => num >= 0);
+  if (!Array.isArray(arr)) throw new Error("Input must be an array");
+  return arr.filter((num) => num >= 0);
 }
 
 module.exports = { alternateCase, productOfArray, filterNegatives };
